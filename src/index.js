960e8d163d4bc09eb1b71a7a884ea30f73a016af
joaopa24 <joaokn129@gmail.com>
2022-02-02 10:13:50 -0300

    types of params

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const express = require('express')
 
 const app = express();
 
+app.use(express.json())
+
 /**
  * Get - busca uma informação dentro do servidor
  * Post - Inserir uma informação no servidor
@@ -11,24 +13,41 @@ const app = express();
  * Delete - Deleta uma informação do servidor
  */
 
+/**
+ * Tipos de Parâmetros
+ * 
+ * Routes params => Parâmetros na rota - Identificar um recurso editar, deletar ou buscar
+ * Query Params => Paginação / Filtro
+ * Body Params => Os objetos inserção/alteração (Json)
+ */
+
 app.get("/courses", (request, response) => {
+    const query = request.query
+    console.log(query)
+
     return response.json(["Curso 1", "Curso 2", "Curso 3"])
 })
 
 app.post("/courses", (request, response) => {
+    const body = request.body
+    console.log(body)
+
     return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
 })
 
 app.put("/courses/:id", (request, response) => {
+    const { id } = request.params;
+    console.log(id)
+
     return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
 })
 
-app.patch("/courses/:id", (request, reponse) => {
+app.patch("/courses/:id", (request, response) => {
     return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"])
 })
 
 app.delete("/courses/:id", (request, response) => {
-    return reponse.json(["Curso 6", "Curso 2", "Curso 4"])
+    return response.json(["Curso 6", "Curso 2", "Curso 4"])
 })
 
 app.listen(3333)
