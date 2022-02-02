33b5752ccc52cdbba25596843da3e3384211d1c0
joaopa24 <joaokn129@gmail.com>
2022-02-01 23:56:22 -0300

    nodemon

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express();
 
 app.get("/", (request, response) => {
-    return response.json({ message: "Hello World! NodeJs" })
+    return response.json({ message: "Hello World! NodeJs - Trilha" })
 })
 
-app.listen(5000)
+app.listen(3333)
